6c425b18e8a66a65e707d975e4e7d4dcd5f2a791
WJMac <Bill.MacGillivray@dal.ca>
2024-11-02 17:54:47 -0300

    ANSI for DEC Line Drawing Mode

--- include/vterminal.h
+++ include/vterminal.h
@@ -18,6 +18,10 @@
 #define CUB(n)      printf(CSI "%dD", (n)); //cursor backward (left) by n
 #define CUP(x,y)    printf(CSI "%d;%dH", (y), (x)); //move cursor to position
 
+//ANSI escape sequence for DEC Line Drawing Mode
+#define EDLDM       printf(ESC "(0"); //starts DEC Line Drawing Mode
+#define EAM         printf(ESC "(B"); //starts ASCII Mode
+
 
 enum keyCodes
 {
