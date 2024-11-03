65e13004b664c95e0f3df89dd4e68110c814e386
WJMac <Bill.MacGillivray@dal.ca>
2024-11-02 22:59:32 -0300

    credit

--- include/vterminal.h
+++ include/vterminal.h
@@ -1,3 +1,8 @@
+/*
+*  Credit is given to Dr. Larry Hughes for providing the reference code
+*  that was used throughout this program specifically the DEC Line Drawing Mode
+*  which uses much of Dr. Hughes' work.
+*/
 #ifndef VTERMINAL_H
 #define VTERMINAL_H
 
@@ -11,18 +16,37 @@
 #define _CRT_SECURE_NO_WARNINGS
 
 //ANSI escape sequence for Control Sequence Introducer for terminal controls
-#define CSI "\x1B["
+#define ESC "\x1b"
+#define CSI "\x1b["
 #define CUU(n)      printf(CSI "%dA", (n)); //cursor up by n
 #define CUD(n)      printf(CSI "%dB", (n)); //cursor down by n
 #define CUF(n)      printf(CSI "%dC", (n)); //cursor forward (right) by n
 #define CUB(n)      printf(CSI "%dD", (n)); //cursor backward (left) by n
 #define CUP(x,y)    printf(CSI "%d;%dH", (y), (x)); //move cursor to position
 
-//ANSI escape sequence for DEC Line Drawing Mode
-#define ESC "\x1B"
-#define EDLDM       printf(ESC "(0"); //starts DEC Line Drawing Mode
-#define EAM         printf(ESC "(B"); //starts ASCII Mode
+//Credit to Dr. Hughes for the following code: lines 28 - 48
+/* Some commonly used VT-100 commands */
+#define EL(r)		printf(CSI "%d;1H" CSI "K", (r)); /* Erase in Line */
+#define CLRSCR		printf(CSI "2J");
 
+/* Cursor keys */
+#define KPNM	printf(ESC ">");	/* Enable Keypad Numeric Mode '[' */
+#define ECKM	printf(CSI "?1h");	/* Enable Cursor Keys Application Mode 'O' */
+#define DCKM	printf(CSI "?1l");	/* Disable Cursor Keys Application Mode */
+#define EDLDM	printf(ESC "(0");	/* Enable DEC Line Drawing Mode */
+#define EAM		printf(ESC "(B");	/* Enable ASCII Mode */
+
+/* DEC colour: ESC [ <n> m */
+enum VT100_COLOURS {
+    FGBLACK = 30,
+    FGRED, FGGREEN, FGYELLOW, FGBLUE, FGMAGENTA, FGCYAN, FGWHITE,
+    BGBLACK = 40,
+    BGRED, BGGREEN, BGYELLOW, BGBLUE, BGMAGENTA, BGCYAN, BGWHITE
+};
+
+/* DEC line drawing mode characters*/
+enum symbol { LR, UR, UL, LL, XX, HR, TR, TL, TU, TD, VR, EM };
+//End of credited material.
 
 enum keyCodes
 {
