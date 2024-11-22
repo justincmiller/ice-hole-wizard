5936a0654db7a5c820f6d3e79a16c51c56aced79
WJMac <Bill.MacGillivray@dal.ca>
2024-11-22 13:21:27 -0400

    Update engine.h

--- include/engine.h
+++ include/engine.h
@@ -6,20 +6,19 @@
 #ifndef ENGINE_H
 #define ENGINE_H
 
-//include the vterminal header for terminal control and key codes
-#include "utils.h"
-#include "style.h"
-#include "map.h"
-#include "display.h"
-#include "cursor.h"
+//include headers
+#include "utils.h" //VT100 I/O & link-lists
+#include "style.h" //menu formatting
+#include "map.h" //layers, map, cells, etc.
+#include "display.h" //system information and menus
+#include "cursor.h" //cursor movement
 
 //function declarations
 void init(); //initializes console setup
 int status(); //gets current state
-void pollKbInput();
-
+void pollKbInput(); //checks to see if key was pressed
 void drawControls(const int code); //handles draw state operations
 void moveControls(const int code); //handles move state operations
-void editControls(const int code);
+void editControls(const int code); //handles edit state operations
 
 #endif
